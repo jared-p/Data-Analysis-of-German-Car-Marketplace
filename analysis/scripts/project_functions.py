aff42e066c554b7a9fe9c65ded26f3f88bdb6a35
jared-p <jaredpaul00@gmail.com>
2021-07-28 17:37:19 -0700

    Added df script file to dir

---
+++ analysis/scripts/project_functions.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib as plt
+
+def load_and_process(path):
+    df = (
+        pd.read_csv(path)
+        .pipe(lambda x: x.drop(x.loc[lambda row : row["gear"].isna()].index))
+        .reset_index(drop = "true")
+        .pipe(lambda x: x.drop(x.loc[lambda row : row["hp"].isna()].index))
+        .reset_index(drop = "true")
+        .pipe(lambda x: x.drop(x.loc[lambda row : row["make"] == "Others"].index))
+        .reset_index(drop = "true")
+        .assign(transmission = lambda x : x.gear)
+        .drop("gear", axis = "columns")
+        .reindex(columns = ["mileage", "fuel", "model", "fuel", "transmission", "offerType", "price", "hp", "year"])
+    )
+    return df
